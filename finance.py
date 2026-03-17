8025ac75023f3770fcb48ab7c79bb2e41e659a08
Mateus Nunes <130379471+MtsNunes@users.noreply.github.com>
2026-03-17 19:49:37 -0300

    iniciando simulador de investimentos

--- finance.py
+++ finance.py
@@ -11,3 +11,23 @@ def adicionar_gasto(gastos):
 
 def calcular_saldo(salario, gastos):
     return salario - gastos
+
+def simular_investimento():
+    invest_mes = 0.0
+    tempo = 0
+    conta = 0.0
+    total_invest = 0.0
+    tempo_total = 0
+    op = 's'
+    while op != 'n':
+        invest_mes = float(input("Quanto você quer investir: "))
+        tempo = int(input("Quantidade de meses investindo: "))
+        tempo_total += tempo
+        for i in range(tempo):
+            conta += invest_mes + conta * 0.01
+        total_invest += invest_mes * tempo
+        print(f"\nTotal final: R$ {conta:.2f}")
+        print(f"Total investido: R$ {total_invest:.2f}")
+        print(f"Tempo investido: {tempo_total/12:.0f} ano(s) e " f"{tempo_total%12:.0f} mes(es)")
+        print(f"Valorizou: R$ {conta-total_invest:.2f}\n")
+        op = input("Quer continuar investindo?(s/n): ")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from finance import adicionar_salario, adicionar_gasto, calcular_saldo
+from finance import adicionar_salario, adicionar_gasto, calcular_saldo, simular_investimento
 
 salario = 0
 gastos = 0
@@ -7,29 +7,41 @@ while True:
     print("\n1 - Adicionar salário")
     print("2 - Adicionar gasto")
     print("3 - Ver saldo")
-    print("4 - Sair")
-    print("5 - Ver resumo")
+    print("4 - Ver resumo")
+    print("5 - Simular investimento\n")
+    print("6 - Sair")
 
     opcao = int(input("Escolha: "))
+    print()
 
     if opcao == 1:
         salario = adicionar_salario()
+        print("\n")
+
     elif opcao == 2:
         gastos = adicionar_gasto(gastos)
+        print("\n")
+    
     elif opcao == 3:
         saldo = calcular_saldo(salario, gastos)
-        print(f"Saldo: R$ {saldo:.2f}")
+        print(f"Saldo: R$ {saldo:.2f}\n")
         if saldo < 0:
-            print("Você está no vermelho!")
+            print("Você está no vermelho!\n")
+    
     elif opcao == 4:
-        print("Saindo...")
-        break
-    elif opcao == 5:
         saldo = calcular_saldo(salario, gastos)
         print(f"Salário: R$ {salario:.2f}")
         print(f"Gastos: R$ {gastos:.2f}")
-        print(f"Saldo: R$ {saldo:.2f}")
+        print(f"Saldo: R$ {saldo:.2f}\n")
         if saldo < 0:
-            print("Você está no vermelho!")
+            print("Você está no vermelho!\n")
+    
+    elif opcao == 5:
+        simular_investimento()
+
+    elif opcao == 6:
+        print("Saindo...")
+        break
+    
     else:
         print("Opção inválida")
